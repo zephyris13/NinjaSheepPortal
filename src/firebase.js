236cfc4cc876fcb1aa693e48de9ce64da0ec1b24
Zephyris13 <gaffer.tape@gmail.com>
2020-09-09 00:33:51 +0200

    Yet Another Fix Attempt

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,10 @@ import 'firebase/firestore'
 const environment = process.env.VUE_APP_DEVELOPMENT;
 console.log(environment);
 
+var db = {};
+var auth = {};
+var usersCollection = {};
+
 if (environment !== undefined) {
   console.log('Development mode');
   
@@ -20,18 +24,25 @@ if (environment !== undefined) {
 
   // firebase init
   firebase.initializeApp(firebaseConfig)
+
+  // utils
+  db = firebase.firestore()
+  auth = firebase.auth()
+
+  // collection references
+  usersCollection = db.collection('users')
 } else {
   fetch('/__/firebase/init.json').then(async response => {
     firebase.initializeApp(await response.json());
+
+    // utils
+    db = firebase.firestore()
+    auth = firebase.auth()
+
+    // collection references
+    usersCollection = db.collection('users')
   });
 }
-
-// utils
-const db = firebase.firestore()
-const auth = firebase.auth()
-
-// collection references
-const usersCollection = db.collection('users')
 
 // export utils/refs
 export {
