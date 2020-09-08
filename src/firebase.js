a54b409f1e5aa3d95e29adbd01623e6dc723afb2
Zephyris13 <gaffer.tape@gmail.com>
2020-09-08 23:15:15 +0200

    Attempted fix for Firebase environment variables issue.

--- src/firebase.js
+++ src/firebase.js
@@ -2,24 +2,30 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-// firebase init
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID
+var firebaseConfig = {};
+
+fetch('/__/firebase/init.json').then(async response => {
+  firebaseConfig = await response.json();
+});  
+
+if (firebaseConfig === undefined || firebaseConfig.apiKey === undefined) {
+  console.log('Development mode');
+
+  firebaseConfig = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID
+  }
+} else {
+  console.log('Production mode');
 }
 
-if (firebaseConfig.apiKey === undefined) {
-  fetch('/__/firebase/init.json').then(async response => {
-    firebase.initializeApp(await response.json());
-  });  
-} else {
-  firebase.initializeApp(firebaseConfig)
-}
+// firebase init
+firebase.initializeApp(firebaseConfig)
 
 // utils
 const db = firebase.firestore()
